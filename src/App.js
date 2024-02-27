a8260c153a69153896831fd036fbe3fdcabcdbd7
retardedE <par.emad@yahoo.com>
2024-02-27 15:10:50 -0800

    create react routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import Home from "./Pages/Home";
+import Product from "./Pages/Product";
+import ProductDetails from "./Pages/ProductDetails";
+import LoginRegister from "./Pages/LoginRegister";
+import PageNotFound from "./Pages/404";
+import AuthContext from "./utils/AuthContext";
+import { Box } from "@mui/material";
+export default function App() {
+  const [token, setToken] = useState();
+  const handleToken = (e) => {
+    setToken(e);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AuthContext.Provider value={{ token, handleToken }}>
+        <Navbar />
+        <Box>
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="/products" element={<Product />}></Route>
+            <Route
+              path="/product-details/:id/:name"
+              element={
+                token ? <ProductDetails /> : <Navigate to={"/login-register"} />
+              }
+            ></Route>
+            <Route
+              path="login-register"
+              element={token ? <Navigate to={"/"} /> : <LoginRegister />}
+            ></Route>
+            <Route path="*" element={<PageNotFound />}></Route>
+          </Routes>
+        </Box>
+        <Footer />
+      </AuthContext.Provider>
+    </>
   );
 }
-
-export default App;
